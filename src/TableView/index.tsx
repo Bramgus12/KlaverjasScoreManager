cfba533ca7db34a7a5f9ebc4aa619a55db65baa9
Bram Gussekloo <bram.gussekloo@gmail.com>
2022-09-06 22:18:05 +0200

    add more table content

--- src/TableView/index.tsx
+++ src/TableView/index.tsx
@@ -1,95 +1,101 @@
 import { StyleSheet, View } from "react-native";
-import { Text } from "react-native-paper";
+import { MD3Theme, Text, withTheme } from "react-native-paper";
 
-function TableView() {
+function TableView(props: { theme: MD3Theme }) {
+    const { colors } = props.theme;
+
+    console.log(colors);
+    
+
+    const styles = StyleSheet.create({
+        column: {
+            height: "100%",
+            width: "80%",
+            display: "flex",
+            flexDirection: "column",
+        },
+        numberColumn: {
+            width: "10%",
+            display: "flex",
+            flexDirection: "column",
+            height: "100%",
+        },
+        topRow: {
+            height: 40,
+            display: "flex",
+            justifyContent: "center",
+            backgroundColor: colors.primary,
+        },
+        row: {
+            backgroundColor: colors.primaryContainer,
+        },
+        topRowNumber: {
+            height: 40,
+            backgroundColor: colors.primary,
+        },
+        text: {
+            textAlign: "center",
+        },
+        textHidden: {
+            backfaceVisibility: "hidden",
+        },
+        divider: {
+            height: "100%",
+            width: 1,
+            backgroundColor: "grey"
+        },
+        container: { 
+            flex: 1, 
+            alignItems: "center", 
+            justifyContent: "center", 
+            display: "flex", 
+            flexDirection: "row",
+            padding: 90,
+        }
+    })
+
     return (
         <View style={styles.container}>
             <View style={styles.numberColumn}>
-                <View style={styles.topRowNumber} />
-                <View style={styles.row}>
-                    <Text variant="headlineSmall">
-                        1
-                    </Text>
-                </View>
+                <View style={{...styles.topRowNumber, borderTopLeftRadius: 10}} />
+                {Array.from(Array(10).keys()).map((number) => (
+                    <View style={styles.row} key={number}>
+                        <Text variant="headlineSmall" style={styles.text}>
+                            {number}
+                        </Text>
+                    </View>
+                ))}
             </View>
-            <View style={styles.divider} />
             <View style={styles.column}>
                 <View style={styles.topRow}>
-                    <Text variant="headlineMedium" style={styles.text}>
+                    <Text variant="headlineMedium" style={{...styles.text, color: "white"}}>
                         Wij
                     </Text>
                 </View>
-                <View style={styles.row}>
-                    <Text variant="headlineSmall" style={styles.text}>
-                        100
-                    </Text>
-                </View>
+                {[10, 20, 30, 40, 50, 60, 70, 80, 90, 100].map((number) => (
+                    <View style={styles.row} key={number}>
+                        <Text variant="headlineSmall" style={styles.text}>
+                            {number}
+                        </Text>
+                    </View>
+                ))}
             </View>
-            <View style={styles.divider} />
             <View style={styles.column}>
-                <View style={styles.topRow}>
-                    <Text variant="headlineMedium" style={styles.text}>
+                <View style={{...styles.topRow, borderTopRightRadius: 10}}>
+                    <Text variant="headlineMedium" style={{ ...styles.text, color: "white" }}>
                         Zij
                     </Text>
                 </View>
-                <View style={styles.row}>
-                    <Text variant="headlineSmall" style={styles.text}>
-                        100
-                    </Text>
-                </View>
+                {[10, 20, 30, 40, 50, 60, 70, 80, 90, 100].map((number) => (
+                    <View style={styles.row} key={number}>
+                        <Text variant="headlineSmall" style={styles.text}>
+                            {number}
+                        </Text>
+                    </View>
+                ))}
             </View>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    column: {
-        height: "100%",
-        width: "80%",
-        display: "flex",
-        flexDirection: "column",
-    },
-    numberColumn: {
-        width: "10%",
-        display: "flex",
-        flexDirection: "column",
-        height: "100%",
-    },
-    topRow: {
-        borderBottomColor: "grey",
-        borderBottomWidth: 1,
-        height: 40,
-        display: "flex",
-        justifyContent: "center"
-    },
-    row: {
-        borderBottomColor: "grey",
-        borderBottomWidth: 1,
-    },
-    topRowNumber: {
-        borderBottomColor: "grey",
-        borderBottomWidth: 1,
-        height: 40,
-    },
-    text: {
-        textAlign: "center",
-    },
-    textHidden: {
-        backfaceVisibility: "hidden",
-    },
-    divider: {
-        height: "100%",
-        width: 1,
-        backgroundColor: "grey"
-    },
-    container: { 
-        flex: 1, 
-        alignItems: "center", 
-        justifyContent: "center", 
-        display: "flex", 
-        flexDirection: "row",
-        padding: 90,
-    }
-})
-
-export default TableView;
+export default withTheme(TableView);
